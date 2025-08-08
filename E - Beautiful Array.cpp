1bc1a693b4629f127062c68646485414331ca007
KJ0905 <kapsm234@gmail.com>
2025-08-08 20:57:22 +0530

    2024 ICPC Asia Taichung Regional Contest

---
+++ E - Beautiful Array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+//#include <math>
+//#include <string>
+using namespace std;
+#define ll long long
+#define ld long double
+#define forn(j,m) for(int i=j;i<m;i++)
+int mini=INT_MAX;
+int maxi=INT_MIN;
+ll lmini=INT64_MAX;
+ll lmaxi=INT64_MIN;
+#define mod 1000000007
+#define inf 1e9
+//freopen("guess.in","r",stdin);   freopen("billboard.out","w",stdout);
+#define all(x) (x).begin(), (x).end()  //sort(all(vec)) instead of sort(vec.begin(), vec.end()).
+#define al(x,n) x,x+n
+#define KUNAL ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+int main(){
+    KUNAL
+        int a,b; cin>>a>>b;
+        cout<<3<<endl;
+        cout<<b<<" "<<b<<" "<<3*a-2*b<<endl;
+}
